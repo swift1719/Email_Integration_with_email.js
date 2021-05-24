ea021bb48f4e8050635d00219dda5d4d510bd6cb
swift1719 <ayushpawar1501@gmail.com>
2021-05-24 23:29:16 +0530

    added emailer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import emailjs from 'emailjs-com';
 
 function App() {
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_u75or3h', 'template_0othe6k', e.target, 'user_iDIpf9LC7LEKjk5KBZCa1')
+      .then((result) => {
+          // console.log(result.text);
+          alert("Email sent successfully!!!")
+      }, (error) => {
+          alert("Error "+error)
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div style={{marginTop:"5%"}}>
+          <form onSubmit={sendEmail} >
+            <div className="form-group">
+              <label>Your Name</label>
+              <input name="name" type="text" className="form-control"
+              placeholder="Enter your name "/>
+            </div>
+            <div className="form-group">
+              <label>Your Email</label>
+              <input name="email" type="email" className="form-control"
+              placeholder="Enter your email "/>
+            </div>
+            <div className="form-group">
+              <label>Message</label>
+              <textarea name="message" className="form-control"
+              placeholder="Enter your message">
+              </textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Send Email
+            </button>
+          </form>
+        </div>
     </div>
   );
 }
